28d5732225377c039e7930af25750c1330acb875
RenanMarceloDev <renanmarcelodev@gmail.com>
2024-01-04 18:18:10 -0300

    NavBar Behaviour finished;

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,15 +1,36 @@
 $(document).ready(function () {
+    const navbar = $('.navbar-custom');
+    const navbarHeight = navbar.outerHeight();
+    const navbarOriginalColor = getComputedStyle(document.documentElement).getPropertyValue('--navbar-color');
+    const navbarScrolledColor = getComputedStyle(document.documentElement).getPropertyValue('--intro-background-color');
+    
+    function updateNavbarColor() {
+        const introSection = $('.intro-section');
+        const introSectionEnd = introSection.offset().top + introSection.outerHeight();
+        const scrollPosition = $(window).scrollTop();
+
+        if (scrollPosition > (introSectionEnd - navbarHeight)) {
+            navbar.css('background-color', navbarScrolledColor);
+        } else {
+            navbar.css('background-color', navbarOriginalColor);
+        }
+    }
+
     $("a").on('click', function (event) {
-        if (this.hash !== "") {
+        const hash = this.hash;
+
+        if (hash !== "") {
             event.preventDefault();
-            
-            var hash = this.hash;
-            
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                window.location.hash = hash;
+            const targetScrollTop = hash === "#intro" ? 0 : $(hash).offset().top;
+
+            $('html, body').animate({ scrollTop: targetScrollTop }, 800, function () {
+                if (hash !== "#intro") {
+                    window.location.hash = hash;
+                }
+                updateNavbarColor();
             });
         }
     });
+
+    $(window).scroll(updateNavbarColor);
 });
